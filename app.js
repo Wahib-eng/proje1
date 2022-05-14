67a7f53b3f66d561bf854e0df603766f0dff3d15
WAHIB MOQBEL <75446834+Wahib-eng@users.noreply.github.com>
2022-05-14 23:36:33 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ window.addEventListener("keydown", (e) => {
   if (e.key == "ArrowLeft" && left > 0) {
     aircraft.style.left = left - 10 + "px";
   }
-  //460  =>  battle width - aircraft width
+  
   else if (e.key == "ArrowRight" && left <= 460) {
     aircraft.style.left = left + 10 + "px";
   }
 
   if (e.key == "ArrowUp" || e.keyCode == 32) {
-    //32 is for space key
+
     var bullet = document.createElement("div");
     bullet.classList.add("bullets");
     battle.appendChild(bullet);
@@ -26,8 +26,7 @@ window.addEventListener("keydown", (e) => {
           var enemyBound = enemy.getBoundingClientRect();
           var bulletbound = bullet.getBoundingClientRect();
 
-          //Condition to check whether the enemy/alien and the bullet are at the same position..!
-          //If so,then we have to destroy that enemy
+          
 
           if (
             bulletbound.left >= enemyBound.left &&
@@ -35,8 +34,8 @@ window.addEventListener("keydown", (e) => {
             bulletbound.top <= enemyBound.top &&
             bulletbound.bottom <= enemyBound.bottom
           ) {
-            enemy.parentElement.removeChild(enemy); //Just removing that particular enemy;
-            //Score in battle
+            enemy.parentElement.removeChild(enemy);;
+          
             document.getElementById("scores").innerHTML =
               parseInt(document.getElementById("scores").innerHTML) + 1;
           }
@@ -46,7 +45,7 @@ window.addEventListener("keydown", (e) => {
         window.getComputedStyle(bullet).getPropertyValue("bottom")
       );
 
-      //Stops the bullet from moving outside the gamebox
+     
       if (bulletbottom >= 500) {
         clearInterval(movebullet);
       }
@@ -60,11 +59,11 @@ window.addEventListener("keydown", (e) => {
 var generateEnemies = setInterval(() => {
   var enemy = document.createElement("div");
   enemy.classList.add("enemies");
-  //Just getting the left of the enemy to place it in random position...
+  
   var enemyLeft = parseInt(
     window.getComputedStyle(enemy).getPropertyValue("left")
   );
-  //generate value between 0 to 450 where 450 => battle width - enemy width
+
   enemy.style.left = Math.floor(Math.random() * 450) + "px";
 
   battle.appendChild(enemy);
@@ -75,12 +74,12 @@ var moveEnemies = setInterval(() => {
 
   if (enemies != undefined) {
     for (var i = 0; i < enemies.length; i++) {
-      //Now I have to increase the top of each enemy,so that the enemies can move downwards..
-      var enemy = enemies[i]; //getting each enemy
+    
+      var enemy = enemies[i]; 
       var enemytop = parseInt(
         window.getComputedStyle(enemy).getPropertyValue("top")
       );
-      //475 => battle height - rockheight + 25
+
       if (enemytop >= 450) {
         alert("Game Over");
         
