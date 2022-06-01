73fd1d7778dfc7774a173e427260d0e07eae7cbd
WAHIB MOQBEL <75446834+Wahib-eng@users.noreply.github.com>
2022-06-01 18:29:09 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,92 +1,83 @@
-var aircraft = document.getElementById("aircraft");
-var battle = document.getElementById("battle");
-
-window.addEventListener("keydown", (e) => {
-  var left = parseInt(window.getComputedStyle(aircraft).getPropertyValue("left"));
-  if (e.key == "ArrowLeft" && left > 0) {
-    aircraft.style.left = left - 10 + "px";
-  }
- 
-  else if (e.key == "ArrowRight" && left <= 460) {
-    aircraft.style.left = left + 10 + "px";
-  }
-
-  if (e.key == "ArrowUp" || e.keyCode == 32) {
-    //32 is for space key
-    var bullet = document.createElement("div");
-    bullet.classList.add("bullets");
-    battle.appendChild(bullet);
-
-    var movebullet = setInterval(() => {
-      var enemies = document.getElementsByClassName("enemies");
-
-      for (var i = 0; i < enemies.length; i++) {
-        var enemy = enemies[i];
-        if (enemy != undefined) {
-          var enemyBound = enemy.getBoundingClientRect();
-          var bulletbound = bullet.getBoundingClientRect();
-
-         
-
-          if (
-            bulletbound.left >= enemyBound.left &&
-            bulletbound.right <= enemyBound.right &&
-            bulletbound.top <= enemyBound.top &&
-            bulletbound.bottom <= enemyBound.bottom
-          ) {
-            enemy.parentElement.removeChild(enemy); 
-            document.getElementById("scores").innerHTML =
-              parseInt(document.getElementById("scores").innerHTML) + 1;
-          }
-        }
-      }
-      var bulletbottom = parseInt(
-        window.getComputedStyle(bullet).getPropertyValue("bottom")
-      );
-
-     
-      if (bulletbottom >= 500) {
-        clearInterval(movebullet);
-      }
-
-      bullet.style.left = left + "px"; 
-      bullet.style.bottom = bulletbottom + 3 + "px";
-    });
-  }
-});
-
-var generateEnemies = setInterval(() => {
-  var enemy = document.createElement("div");
-  enemy.classList.add("enemies");
-  
-  var enemyLeft = parseInt(
-    window.getComputedStyle(enemy).getPropertyValue("left")
-  );
-
-  enemy.style.left = Math.floor(Math.random() * 450) + "px";
-
-  battle.appendChild(enemy);
-}, 1000);
-
-var moveEnemies = setInterval(() => {
-  var enemies = document.getElementsByClassName("enemies");
-
-  if (enemies != undefined) {
-    for (var i = 0; i < enemies.length; i++) {
-
-      var enemy = enemies[i];
-      var enemytop = parseInt(
-        window.getComputedStyle(enemy).getPropertyValue("top")
-      );
-    
-      if (enemytop >= 450) {
-        alert("Game Over");
-        
-        clearInterval(moveEnemies);
-        window.location.reload();
-      }
-
-      enemy.style.top = enemytop + 25 + "px";
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>aircraft war</title>
+  </head>
+  <style>
+    body {
+      width: 100%;
+      overflow: hidden;
+      background: url("space2.jpg");
+      background-size: cover;
     }
-  }
-}, 450);
+
+    h1 {
+      text-align: center;
+      color: white;
+      font-weight: bold;
+      margin: 0;
+      margin-top: 10px;
+    }
+    #battle {
+      position: relative;
+      width: 500px;
+      height: 500px;
+      margin: 30px auto;
+      border: 4px solid white;
+      border-style: groove;
+      background-color: rgb(41, 6, 46);
+      overflow: hidden;
+    }
+
+    #aircraft {
+      position: absolute;
+      width: 50px;
+      height: 50px;
+      background: url("ship.png");
+      background-size: contain;
+      background-repeat: no-repeat;
+      left: 50%;
+      bottom: 3px;
+    }
+
+    .bullets {
+      position: absolute;
+      bottom: 40px;
+      left: 50%;
+      width: 20px;
+      height: 20px;
+      background: url("bullet.png");
+      background-size: contain;
+      background-repeat: no-repeat;
+    }
+
+    .enemies {
+      position: absolute;
+      top: 0px;
+      width: 50px;
+      height: 50px;
+      background: url("alien1.png");
+      background-size: contain;
+      background-repeat: no-repeat;
+    }
+    #scores {
+      position: absolute;
+      top: 0px;
+      margin: 70px;
+      opacity: 1;
+      font-size: 10em;
+      color: white;
+    }
+  </style>
+  <script src="app.js" defer></script>
+  <body>
+    <h1>AIRCRAFT WAR</h1>
+    <div id="battle">
+      <div class="enemies"></div>
+      <div id="aircraft"></div>
+    </div>
+    <div id="scores">0</div>
+  </body>
+</html>
